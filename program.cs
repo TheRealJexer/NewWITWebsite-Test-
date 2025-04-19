af2f14f162b7c18b007941ae00b045b8af00cc2c
TheRealJexer <jason.d.dyck@edu.sait.ca>
2025-04-19 03:35:48 -0500

    test

---
+++ program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); // Enables index.html as the default
+app.UseStaticFiles();  // Serves static files from wwwroot
+
+app.Run();
